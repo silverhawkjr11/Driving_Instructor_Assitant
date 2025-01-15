31367b95e50be9cf44de4b0258e352e78a83de05
silverhawkjr11 <saeed.kanawat@gmail.com>
2025-01-15 12:12:54 +0200

    without app header

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,3 @@
-// export const environment = {
-//   production: false,
-//   firebaseConfig: {
-//     apiKey: "AIzaSyAul551P-Gx15AZENjQse_2uSWDRn9qAjQ",
-//     authDomain: "store-4b4c6.firebaseapp.com",
-//     databaseURL: "https://store-4b4c6-default-rtdb.firebaseio.com",
-//     projectId: "store-4b4c6",
-//     storageBucket: "store-4b4c6.appspot.com",
-//     messagingSenderId: "1023781380734",
-//     appId: "1:1023781380734:web:8159635e61658da6cf2e2f",
-//     measurementId: "G-VQXBF3L961"
-//   }
-// };
-
 export const environment = {
   production: false,
   firebaseConfig: {
